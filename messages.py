a8159ce21caffa97d58ecab166106165a9a6877f
Karasik-piiiip <77832587+Karasik-piiiip@users.noreply.github.com>
2021-07-14 21:23:01 +1000

    Update messages.py
    Обновил функцию отправки картинки, добавил клавиатуру, добавил базу данных пользователей для возможности рассылки.

--- messages.py
+++ messages.py
@@ -1,14 +1,15 @@
 """
 Бот для ВКонтакте, отправляющий случайные картинки из своих альбомов. Адрес: https://vk.com/picturerand
 """
-
 import vk_api
 from vk_api.utils import get_random_id
 from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 
 from random import randint as rnd
-
-import datetime
+import logging
+import sqlite3
+import re
 
 token = ' ' # Токен от группы ВК.
 ses = vk_api.VkApi(token = token)
@@ -17,46 +18,76 @@ longpoll = VkLongPoll(ses)
 
 owner_id = ' ' # id сообщества( со знаком минуса в начале).
 
-def server_info(user_id, photo):
-    """
-    Функция для ведения журнала сервера.
-    """
-    men = vk.users.get(user_ids = user_id)
-    men = men[0]
-    info = '{0} {1} получил картинку {2} в {3}'.format(men['first_name'], men['last_name'], str(photo), str(datetime.datetime.now()))
-    print(info)
+k_board = VkKeyboard() # Клавиатура.
+k_board.add_button('/pic', color=VkKeyboardColor.POSITIVE, payload={"type": "pic"})
 
-def generate_num(min, max, isk):
-    """
-    Та же функция randint(), только не включающая числа из кортежа <isk>.
-    """
-    result = rnd(min, max)
-    if str(result) in isk:
-       result = generate_num(min, max, isk)
-    return result
+def table(user_id = None, chat_id = None):
+	"""Добавляем пользователя или чат в БД, если их там нет """
+    cur = con.cursor()
+    cur.execute('create table if not exists Users (id INTEGER UNIQUE)')
+    cur.execute('create table if not exists Chats (id INTEGER UNIQUE)')
+    if user_id != None:
+        cur.execute('insert or ignore into Users values(?)', (user_id,))
+        con.commit()
+    if chat_id != None:
+        cur.execute('insert or ignore into Chats values(?)', (chat_id,))
+        con.commit()
+    cur.close()
+
+def get_table():
+    cur = con.cursor()
+    users = [users[0] for users in cur.execute("SELECT id FROM Users")]
+    chats = [chats[0] for chats in cur.execute("SELECT id FROM Chats")]
+    cur.close()
+    return users, chats
+
+def send_mailing(text):
+    users, chats = get_table()
+    for user in users:
+        vk.messages.send(user_id = user, message = text, random_id = get_random_id(), keyboard = k_board.get_keyboard())
+    for chat in chats:
+        vk.messages.send(chat_id = chat, message = text, random_id = get_random_id(), keyboard = k_board.get_keyboard())
+
 
 def send_pic(id):
-    r_num = generate_num(<min>, <max>, [<Исключение_в_формате_строки>]) # Генерируем id фотографии.
-    vk.messages.send(user_id = id, attachment= str('photo' + owner_id + '_' + str(r_num)) , random_id = get_random_id())
-    server_info(id, r_num)
+    """Отправляет картинку в личные сообщения. """
+    try:
+        r_num = rnd(<min>, <max>) # Генерируем id фотографии.
+        vk.messages.send(user_id = id, attachment= str('photo' + owner_id + '_' + str(r_num)) , random_id = get_random_id(), keyboard = k_board.get_keyboard())
+    except Exception:
+        send_pic(id)
 
-def send_pic_chat(chat, user_id):
-    r_num = generate_num(<min>, <max>, [<Исключение_в_формате_строки>]) # Генерируем id фотографии.
-    vk.messages.send(chat_id = chat, attachment= str('photo' + owner_id + '_' + str(r_num)) , random_id = get_random_id())
-    server_info(user_id, r_num)
+def send_pic_chat(chat):
+    """Отправляет картинку в чат. """
+    try:
+        r_num = rnd(<min>, <max>) # Генерируем id фотографии.
+        vk.messages.send(chat_id = chat, attachment= str('photo' + owner_id + '_' + str(r_num)) , random_id = get_random_id(), keyboard = k_board.get_keyboard())
+    except Exception:
+        send_pic_chat(chat)
 
 while True:
     try:
+		logging.info('Программа запущена')
+    	con = sqlite3.connect('base.b')
         for event in longpoll.listen():
             if event.type == VkEventType.MESSAGE_NEW:
                 if event.from_user:
+					table(event.user_id) # Добавляем в базу пользователя.
                     if event.text == '/pic': # Команда.
                         send_pic(event.user_id)
+					if '/ra' in re.findall('/ra', event.text) and event.user_id == <ID_администратора_группы>:
+						# Идея в том, чтобы отправлять рассылку
+						# прямо из ВКонтакте, написан в лс боту.
+                        logging.info('Запрошена команда /ra пользователем %s' % str(event.user_id))
+                        send_mailing(event.text[3:])
                 if event.from_chat:
+					table(chat_id = event.chat_id) # Добавляем в базу беседу.
                     if event.text == '/pic': # Команда.
-                        send_pic_chat(event.chat_id, event.user_id)
-    except Exception:
+                        send_pic_chat(event.chat_id)
+    except Exception as e:
         ### Подразумевается, что будет обработана ошибка 
         ### longpoll, которая переодически вызывается из-за
         ### отсуствия ответа при прослушивании серверов ВКонтакте.
+		logging.error('Произошла ошибка: %s' % str(e))
+        con.close()
         continue
